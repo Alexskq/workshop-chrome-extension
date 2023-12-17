b82f0dce705fcef1ceb6a90e46901fc32a1b5dc2
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2023-12-17 15:12:53 +0100

    Step 29

--- scripts/cheesify.js
+++ scripts/cheesify.js
@@ -1,6 +1,18 @@
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action == "cheesify") {
+    cheesify();
+  }
+});
+
 function cheesify() {
   // TODO: Add the image replacement script here
+
+  document.querySelectorAll("img").forEach((i) => {
+    i.src = `https://source.unsplash.com/${img.width}x${
+      img.height
+    }/?cheese&${Math.random()}`;
+    i.srcset = i.src;
+  });
 }
 
 // TODO: Write a function to listen for messages on the content page using chrome.runtime.onMessage
-  
